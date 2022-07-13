feccbf54d938a8b34244ffda61b68b6dd42b12bd
Awamry <omarelawamry@gmail.com>
2022-07-13 14:43:41 +0200

    Refactor redis_handler_service for chats_count and messages_count keys to store them in hashes to have more reliability in removing them from redis rather than relying on EXPIRE method which could remove the key before flushing its value to the DB

--- app/lib/redis_handler_service.rb
+++ app/lib/redis_handler_service.rb
@@ -3,6 +3,9 @@ class RedisHandlerService
   MESSAGE_NUMBER_KEY = "message_number"
   CHATS_COUNT_KEY = "chats_count"
   MESSAGES_COUNT_KEY = "messages_count"
+  HASH_VALUE_KEY = 'value'
+  HASH_EXPIRATION_DATE_KEY = 'expiration_date'
+  HASH_IS_FLUSHED_TO_DB_KEY = 'is_flushed_to_db'
 
   def self.set(key, value)
     $namespaced_redis.set(key, value)
@@ -25,40 +28,65 @@ class RedisHandlerService
   end
 
   def self.increment_chats_count(application_id)
-    if key_exists?("#{CHATS_COUNT_KEY}:#{application_id}")
-      $namespaced_redis.multi do |multi|
-        multi.incr("#{CHATS_COUNT_KEY}:#{application_id}")
-        multi.expire("#{CHATS_COUNT_KEY}:#{application_id}", 10)
-      end[0]
+    key = "#{CHATS_COUNT_KEY}:#{application_id}"
+    if key_exists?(key)
+      result = update_hash(key)
+      return result[0]
     else
       chats_count = Application.find(application_id).chats.size
-      if key_exists?("#{CHATS_COUNT_KEY}:#{application_id}")
-        $namespaced_redis.multi do |multi|
-          multi.incr("#{CHATS_COUNT_KEY}:#{application_id}")
-          multi.expire("#{CHATS_COUNT_KEY}:#{application_id}", 10)
-        end[0]
-      else
-        $namespaced_redis.multi do |multi|
-          multi.set("#{CHATS_COUNT_KEY}:#{application_id}", chats_count + 1)
-          multi.expire("#{CHATS_COUNT_KEY}:#{application_id}", 10)
-        end
-        return chats_count + 1
+      if key_exists?(key)
+        result = update_hash(key)
+        return result[0]
       end
+      $namespaced_redis.hmset(
+        "#{CHATS_COUNT_KEY}:#{application_id}", HASH_VALUE_KEY,
+        chats_count + 1, HASH_EXPIRATION_DATE_KEY, Time.now.to_i + (3 * 60 * 60),
+        HASH_IS_FLUSHED_TO_DB_KEY, 'false'
+      )
+      return chats_count + 1
     end
   end
 
   def self.increment_messages_count(chat_id)
-    if key_exists?("#{MESSAGES_COUNT_KEY}:#{chat_id}")
-      increment("#{MESSAGES_COUNT_KEY}:#{chat_id}")
+    key = "#{MESSAGES_COUNT_KEY}:#{chat_id}"
+    if key_exists?(key)
+      $namespaced_redis.multi do |multi|
+        result = update_hash(key)
+        return result[0]
+      end
     else
-      messages_count = Chat.find("#{MESSAGES_COUNT_KEY}:#{chat_id}").messages.size
-      if key_exists?("#{MESSAGES_COUNT_KEY}:#{chat_id}")
-        increment("#{MESSAGES_COUNT_KEY}:#{chat_id}")
+      messages_count = Chat.find(chat_id).messages.size
+      if key_exists?(key)
+        result = update_hash(key)
+        return result[0]
       else
-        set("#{MESSAGES_COUNT_KEY}:#{chat_id}", messages_count + 1)
+        $namespaced_redis.hmset(
+          "#{MESSAGES_COUNT_KEY}:#{chat_id}", HASH_VALUE_KEY,
+          chats_count + 1, HASH_EXPIRATION_DATE_KEY, Time.now.to_i + (3 * 60 * 60),
+          HASH_IS_FLUSHED_TO_DB_KEY, 'false'
+        )
         return messages_count + 1
       end
     end
   end
 
+  def self.update_hash(key)
+    $namespaced_redis.multi do |multi|
+      multi.hincrby(key, HASH_VALUE_KEY, 1)
+      multi.hset(key, HASH_EXPIRATION_DATE_KEY, Time.now.to_i + (3 * 60 * 60))
+      multi.hset(key, HASH_IS_FLUSHED_TO_DB_KEY, 'false')
+    end
+  end
+
+  def self.get_chats_count_keys
+    $namespaced_redis.keys("#{CHATS_COUNT_KEY}:*")
+  end
+
+  def self.get_messages_count_keys
+    $namespaced_redis.keys("#{MESSAGES_COUNT_KEY}:*")
+  end
+
+  def self.get_hash_values(key)
+    $namespaced_redis.hmget(key, HASH_VALUE_KEY, HASH_EXPIRATION_DATE_KEY, HASH_IS_FLUSHED_TO_DB_KEY)
+  end
 end
