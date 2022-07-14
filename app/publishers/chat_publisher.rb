3f44d19190d5b5f7d7d77801e45f91097d31c561
Awamry <omarelawamry@gmail.com>
2022-07-14 16:00:08 +0200

    Increase reliability of publishers by persisting the messages sent to exchanges and adding publisher confirms to ensure that the payload has been published successfully

--- app/publishers/chat_publisher.rb
+++ app/publishers/chat_publisher.rb
@@ -5,7 +5,12 @@ class ChatPublisher
       channel = connection.create_channel
       exchange = channel.exchange("chatting_system", durable: true)
       queue = channel.queue("chats", durable: true).bind(exchange, :routing_key => "chats")
-      exchange.publish(chat, :routing_key => queue.name)
+      channel.confirm_select
+      exchange.publish(chat, :routing_key => queue.name, :persistent => true)
+      unless channel.wait_for_confirms
+        channel.close
+        raise StandardError.new "Unable to publish chat"
+      end
       channel.close
     end
   end
--- app/publishers/message_publisher.rb
+++ app/publishers/message_publisher.rb
@@ -1,11 +1,16 @@
 class MessagePublisher
-  def self.publish(chat)
+  def self.publish(message)
     BUNNY.with do |connection|
       connection.start
       channel = connection.create_channel
       exchange = channel.exchange("chatting_system", durable: true)
       queue = channel.queue("messages", durable: true).bind(exchange, :routing_key => "messages")
-      exchange.publish(chat, :routing_key => queue.name)
+      channel.confirm_select
+      exchange.publish(message, :routing_key => queue.name, :persistent => true)
+      unless channel.wait_for_confirms
+        channel.close
+        raise StandardError.new "Unable to publish message"
+      end
       channel.close
     end
   end
